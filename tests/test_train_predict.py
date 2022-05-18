0970999200fe88e6c19e9b60099cc125d32515ab
ezuryy <daria.kidinova@gmail.com>
2022-05-18 19:53:18 +0300

    remove reading models from file

--- tests/test_train_predict.py
+++ tests/test_train_predict.py
@@ -28,7 +28,7 @@ def test_predict(params: TrainingParams):
     assert os.path.exists(params.predict_path)
 
 
-def test_predict_on_real_data():
+def test_accuracy_on_real_data():
     config_path = fix_path("configs/config1.yaml")
     params = fix_config(read_training_params(config_path))
 
@@ -39,10 +39,3 @@ def test_predict_on_real_data():
     assert abs(metrics["F1-score"] - 0.8) <= 0.1
     assert os.path.exists(params.model_path)
     assert os.path.exists(params.preprocess_pipeline_path)
-
-    metrics = run_test_pipeline(params)
-
-    assert abs(metrics["Accuracy"] - 0.8) < 0.1
-    assert abs(metrics["ROC AUC score"] - 0.8) < 0.1
-    assert abs(metrics["F1-score"] - 0.8) < 0.1
-    assert os.path.exists(params.predict_path)
